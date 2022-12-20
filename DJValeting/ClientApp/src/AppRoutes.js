e6925578016bb7bae64fb074af68af953f6f820a
James Goode <james_goode@hotmail.co.uk>
2022-12-20 14:23:00 +0000

    Remove auto-generated weatherforecast components, add bookings page

--- DJValeting/ClientApp/src/AppRoutes.js
+++ DJValeting/ClientApp/src/AppRoutes.js
@@ -1,6 +1,5 @@
-import { Counter } from "./components/Counter";
-import { FetchData } from "./components/FetchData";
 import { Home } from "./components/Home";
+import { Bookings } from "./components/Bookings";
 
 const AppRoutes = [
   {
@@ -8,12 +7,8 @@ const AppRoutes = [
     element: <Home />
   },
   {
-    path: '/counter',
-    element: <Counter />
-  },
-  {
-    path: '/fetch-data',
-    element: <FetchData />
+    path: '/bookings',
+      element: <Bookings />
   }
 ];
 
---
+++ DJValeting/ClientApp/src/components/Bookings.js
@@ -0,0 +1,20 @@
+﻿import React, { Component } from 'react';
+
+export class Bookings extends Component {
+    static displayName = Bookings.name;
+
+    constructor(props) {
+        super(props);
+        this.state = { bookings: [], loading: true };
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Bookings</h1>
+                <table>
+                </table>
+            </div>
+        );
+    }
+}
