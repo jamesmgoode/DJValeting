bbc365e58ff74afcad3a44501669c756aef48b40
James Goode <james_goode@hotmail.co.uk>
2022-12-20 13:01:17 +0000

    Add the booking fields

--- DJValeting/ClientApp/src/components/Home.js
+++ DJValeting/ClientApp/src/components/Home.js
@@ -1,26 +1,58 @@
 import React, { Component } from 'react';
 
 export class Home extends Component {
-  static displayName = Home.name;
+    static displayName = Home.name;
 
-  render() {
-    return (
-      <div>
-        <h1>Hello, world!</h1>
-        <p>Welcome to your new single-page application, built with:</p>
-        <ul>
-          <li><a href='https://get.asp.net/'>ASP.NET Core</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform server-side code</li>
-          <li><a href='https://facebook.github.io/react/'>React</a> for client-side code</li>
-          <li><a href='http://getbootstrap.com/'>Bootstrap</a> for layout and styling</li>
-        </ul>
-        <p>To help you get started, we have also set up:</p>
-        <ul>
-          <li><strong>Client-side navigation</strong>. For example, click <em>Counter</em> then <em>Back</em> to return here.</li>
-          <li><strong>Development server integration</strong>. In development mode, the development server from <code>create-react-app</code> runs in the background automatically, so your client-side resources are dynamically built on demand and the page refreshes when you modify any file.</li>
-          <li><strong>Efficient production builds</strong>. In production mode, development-time features are disabled, and your <code>dotnet publish</code> configuration produces minified, efficiently bundled JavaScript files.</li>
-        </ul>
-        <p>The <code>ClientApp</code> subdirectory is a standard React application based on the <code>create-react-app</code> template. If you open a command prompt in that directory, you can run <code>npm</code> commands such as <code>npm test</code> or <code>npm install</code>.</p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <h1>Create Booking</h1>
+                <div className="form-group row">
+                    <label htmlFor="name" className="col-sm-2 col-form-label">Name</label>
+                    <div className="col-sm-3">
+                        <input type="text" className="form-control" id="name" placeholder="Name" />
+                    </div>
+                </div>
+                <div className="form-group row">
+                    <label htmlFor="date" className="col-sm-2 col-form-label">Booking Date</label>
+                    <div className="col-sm-2">
+                        <input type="date" className="form-control" id="date" />
+                    </div>
+                </div>
+                <div className="form-group row">
+                    <label htmlFor="flexibility" className="col-sm-2 col-form-label">Flexibility</label>
+                    <div className="col-sm-2">
+                        <select id="flexibility" className="form-select">
+                            <option value="1">+/- 1 Day</option>
+                            <option value="2">+/- 2 Days</option>
+                            <option value="3">+/- 3 Days</option>
+                        </select>
+                    </div>
+                </div>
+                <div className="form-group row">
+                    <label htmlFor="vehicle-size" className="col-sm-2 col-form-label">Vehicle Size</label>
+                    <div className="col-sm-2">
+                        <select id="vehicle-size" className="form-select">
+                            <option value="small">Small</option>
+                            <option value="medium">Medium</option>
+                            <option value="large">Large</option>
+                            <option value="van">Van</option>
+                        </select>
+                    </div>
+                </div>
+                <div className="form-group row">
+                    <label htmlFor="contact-number" className="col-sm-2 col-form-label">Contact Number</label>
+                    <div className="col-sm-3">
+                        <input type="tel" className="form-control" id="contact-number" placeholder="Contact Number" />
+                    </div>
+                </div>
+                <div className="form-group row">
+                    <label htmlFor="email" className="col-sm-2 col-form-label">Email Address</label>
+                    <div className="col-sm-3">
+                        <input type="email" className="form-control" id="email" placeholder="Email address" />
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
