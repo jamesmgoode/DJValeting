20b6155b65d0341afe22f0f252fb833a344ce7cf
James Goode <james_goode@hotmail.co.uk>
2022-12-20 13:08:51 +0000

    Add BookingController

---
+++ DJValeting/Controllers/BookingController.cs
@@ -0,0 +1,15 @@
+﻿using DJValeting.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DJValeting.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class BookingController : Controller
+    {
+        public async Task SubmitBooking(BookingSubmission bookingSubmission)
+        {
+
+        }
+    }
+}
