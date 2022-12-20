87f8571bb061729c048096831f9c68974707f15c
James Goode <james_goode@hotmail.co.uk>
2022-12-20 13:07:43 +0000

    Add BookingSubmission model

---
+++ DJValeting/Models/BookingSubmission.cs
@@ -0,0 +1,17 @@
+﻿namespace DJValeting.Models
+{
+    public class BookingSubmission
+    {
+        public string Name { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public int? Flexibility { get; set; }
+
+        public string VehicleSize { get; set; }
+
+        public string ContactNumber { get; set; }
+
+        public string EmailAddress { get; set; }
+    }
+}
